3adae0027c377cd1c52d171b5c0109c7df3fa572
Marcello Magno <Marcello_magno@hotmai.com>
2025-10-21 17:18:07 -0300

    user and admin OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get "welcome/index"
+  end
+  namespace :users_backoffice do
+    get "welcome/index"
+  end
+  namespace :admins_backoffice do
+    get "welcome/index"
+  end
+  get "site/index"
+  devise_for :users
+  devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "site/welcome#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
