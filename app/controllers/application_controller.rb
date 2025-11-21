2be1a983e72a44c7cd2f8ef36da009614d94d2d1
Marcello Magno <Marcello_magno@hotmai.com>
2025-11-21 14:28:52 -0300

    add total questions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   layout :layout_by_resource
+
+  def after_sign_in_path_for(resource)
+    users_backoffice_welcome_index_path
+  end
 =begin
   protected
     def layout_by_resource
@@ -24,11 +28,13 @@ class ApplicationController < ActionController::Base
       end
     end
 =end
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :last_name ])
   end
+
   def layout_by_resource
     devise_controller? ? "#{resource_name}_devise" : "application"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_18_150106) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_21_131256) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -74,6 +74,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_18_150106) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_statistics", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "right_questions", default: 0
+    t.integer "wrong_questions", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_statistics_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -92,4 +101,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_18_150106) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "subjects"
+  add_foreign_key "user_statistics", "users"
 end
